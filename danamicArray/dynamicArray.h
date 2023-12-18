6795f90138838cd184aedd54303dbb2abb5fde90
吕子衡 <you@example.com>
2023-12-18 11:12:52 +0800

    [新增]新增动态数组头文件

---
+++ danamicArray/dynamicArray.h
@@ -0,0 +1,8 @@
+#ifndef __DANAMIC_ARRAY_H_
+#define __DANAMIC_ARRAY_H_
+
+//避免头文件包含
+
+
+
+#endif //__DANAMIC_ARRAY_H_
---
+++ danamicArray/main.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <dynamicArray.h>
+
+int main()
+{
+    /* 静态数组
+         1.不够灵活
+         2.可能会有浪费的现象
+    */
+
+    return 0;
+}
