0554475e65abfd3c56bd415e27738d6532e2d040
吕子衡 <you@example.com>
2023-12-20 15:03:53 +0800

    [修改]完善链表销毁接口

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -171,12 +171,25 @@ int LinkListGetLength(LinkList *pList, int *pSize)
     {
         *pSize = pList->len;
     }
-    return ON_SUCCESS;
+    return pList->len;
 }
 
 // 链表销毁
 int LinkListDestroy(LinkList *pList)
 {
+    int size = 0;
+    while (LinkListGetLength(pList))
+    {
+        LinkListHeadDel(pList);
+    }
+    if (pList->head != NULL)
+    {
+        free(pList->head);
+        // 指针置为NULL
+        pList->head = NULL;
+        pList->tail = NULL;
+    }
+    return ON_SUCCESS;
 }
 
 // 链表遍历
