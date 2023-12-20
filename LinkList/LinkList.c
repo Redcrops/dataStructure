4b950314cfe940be2b45f101a363ce3e42e3f88a
吕子衡 <you@example.com>
2023-12-20 11:15:20 +0800

    [修改]完善链表插入接口

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -11,6 +11,9 @@ enum STATUS_CODE
     INVAILD_ACCESS,
 };
 // 链表初始化
+static int checkPlist(LinkList *pList);
+static int checkPos(LinkList *pList, int pos);
+static int checkMallocNode(LinkNode *Node);
 int LinkListInit(LinkList **pList)
 {
     // 分配内存，判空，初始化
@@ -38,23 +41,67 @@ int LinkListInit(LinkList **pList)
     list->head->next = NULL;
 
     // 二级指针
-    *pList = pList;
+    *pList = list;
     return ON_SUCCESS;
 }
 
 // 链表头插
 int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE val)
 {
+    return LinkListAppointPosInsert(pList, 0, val);
 }
 
 // 链表尾插
 int LinkListTailInsert(LinkList *pList, ELEMENTTYPE val)
 {
+    return LinkListAppointPosInsert(pList, pList->len - 1, val);
 }
-
+static int checkPlist(LinkList *pList)
+{
+    if (pList == NULL)
+    {
+        return NULL_PTR;
+    }
+}
+static int checkPos(LinkList *pList, int pos)
+{
+    if (pos < 0 || pos > pList->len)
+    {
+        return INVAILD_ACCESS;
+    }
+}
+static int checkMallocNode(LinkNode *Node)
+{
+    if (Node == NULL)
+    {
+        return MALLOC_ERROR;
+    }
+    memset(Node, 0, sizeof(LinkNode) * 1);
+}
 // 链表指定位置插入
 int LinkListAppointPosInsert(LinkList *pList, int pos, ELEMENTTYPE val)
 {
+    checkPlist(pList);
+    checkPos(pList, pos);
+    LinkNode *travelNode = pList->head;
+    while (pos)
+    {
+        travelNode = travelNode->next;
+        pos--;
+    }
+    LinkNode *newNode = (LinkNode *)malloc(sizeof(LinkNode) * 1);
+    checkMallocNode(newNode);
+
+    newNode->data = 0;
+    newNode->next = NULL;
+
+    // 修改节点指向
+    newNode->next = travelNode->next;
+    travelNode->next = newNode;
+
+    // 更新链表长度
+    pList->len++;
+    return ON_SUCCESS;
 }
 
 // 链表头删
@@ -80,6 +127,12 @@ int LinkListDelAppointData(LinkList *pList, ELEMENTTYPE val)
 // 获取链表长度
 int LinkListGetLength(LinkList *pList, int *pSize)
 {
+    checkPlist(pList);
+    if (pSize != NULL)
+    {
+        *pSize = pList->len;
+    }
+    return ON_SUCCESS;
 }
 
 // 链表销毁
@@ -90,4 +143,6 @@ int LinkListDestroy(LinkList *pList)
 // 链表遍历
 int linkListforeach(LinkList *pList)
 {
+    checkPlist(pList);
+    
 }
