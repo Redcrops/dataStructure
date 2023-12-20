58ed37a2502d0148f07e5da08e28e4102f6ff4d2
吕子衡 <you@example.com>
2023-12-20 10:14:25 +0800

    [新增]新增接口参数

--- LinkList/LinkList.c
+++ LinkList/LinkList.c
@@ -1,1 +1,51 @@
 #include "LinkList.h"
+
+// 链表初始化
+int LinkListInit(LinkList **pList)
+{
+}
+
+// 链表头插
+int LinkListHeadInsert(LinkList *pList, ELEMENTTYPE val)
+{
+}
+
+// 链表尾插
+int LinkListTailInsert(LinkList *pList, ELEMENTTYPE val)
+{
+}
+
+// 链表指定位置插入
+int LinkListAppointPosInsert(LinkList *pList, int pos, ELEMENTTYPE val)
+{
+}
+
+// 链表头删
+int LinkListHeadDel(LinkList *pList)
+{
+}
+
+// 链表尾删
+int LinkListTailDel(LinkList *pList)
+{
+}
+
+// 链表指定位置删除
+int LinkListDelAppointPos(LinkList *pList, int pos)
+{
+}
+
+// 链表删除指定数据
+int LinkListDelAppointData(LinkList *pList, ELEMENTTYPE val)
+{
+}
+
+// 获取链表长度
+int LinkListGetLength(LinkList *pList, int *pSize)
+{
+}
+
+// 链表销毁
+int LinkListDestroy(LinkList *pList)
+{
+}
--- LinkList/LinkList.h
+++ LinkList/LinkList.h
@@ -20,32 +20,32 @@ typedef struct LinkList
 } LinkList;
 
 // 链表初始化
-int LinkListInit();
+int LinkListInit(LinkList **pList);
 
 // 链表头插
-int LinkListHeadInsert();
+int LinkListHeadInsert(LinkList *pList,ELEMENTTYPE val);
 
 // 链表尾插
-int LinkListTailInsert();
+int LinkListTailInsert(LinkList *pList,ELEMENTTYPE val);
 
 // 链表指定位置插入
-int LinkListAppointPosInsert();
+int LinkListAppointPosInsert(LinkList *pList,int pos, ELEMENTTYPE val);
 
 // 链表头删
-int LinkListHeadDel();
+int LinkListHeadDel(LinkList *pList);
 
 // 链表尾删
-int LinkListTailDel();
+int LinkListTailDel(LinkList *pList);
 
 // 链表指定位置删除
-int LinkListDelAppointPos();
+int LinkListDelAppointPos(LinkList *pList,int pos);
 
-// 删除指定数据
-int LinkListDelAppointData();
+// 链表删除指定数据
+int LinkListDelAppointData(LinkList *pList,ELEMENTTYPE val);
 
 // 获取链表长度
-int LinkListGetLength();
+int LinkListGetLength(LinkList *pList,int *pSize);
 
 // 链表销毁
-int LinkListDestroy();
+int LinkListDestroy(LinkList *pList);
 #endif //__LINKLIST_H_
